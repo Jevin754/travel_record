ae208ad4ca2d25a988acdf954d375d278c4e39d0
vra <wangyf11@mail.ustc.edu.cn>
2016-12-19 15:31:23 +0800

    update view

--- login/views.py
+++ login/views.py
@@ -4,7 +4,7 @@ from django.template import RequestContext
 # Create your views here.
 def login(request):
 	# SET YOUR PASSWORD HERE!
-	your_password = 'your_password'
+	your_password = 'put_your_password_here'
 	if request.method == 'POST':
 		if request.POST['password'] == your_password:
 			return render_to_response('success.html', context_instance=RequestContext(request))
